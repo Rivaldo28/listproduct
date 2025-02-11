6491906fe37e26ee3ae527c2197662c073126d03
Rivaldo Souza <rivaldo.lsouza@outlook.com>
2025-02-11 17:00:04 -0300

    create package of  model user and products

---
+++ src/main/java/com/mvc/model/Product.java
@@ -0,0 +1,29 @@
+package com.mvc.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "products")
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, length = 50)
+    private String name;
+
+    @Column(nullable = false, length = 100)
+    private String description;
+
+    @Column(nullable = false)
+    private float price;
+
+    @Column(nullable = false)
+    private Integer quantity;
+
+    @Column(nullable = false, length = 50)
+    private String status;
+}
